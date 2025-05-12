bc8101ad967539391d3dab325ee155f23dd6de26
Talles <tgtallles362@gmail.com>
2025-05-12 17:36:40 -0300

    desespero

--- Trabalho/questao_two.py
+++ Trabalho/questao_two.py
@@ -1,1 +1,15 @@
+cp_p = 9.00
+cp_m = 14.00
+cp_g = 18.00
+
 print('Bem-Vindo a Loja de Gelados do Talles Robert')
+print(20 * '-', 'Cardápio', 20 * '-')
+print(50 * '-')
+print(f'---| Tamanho  |   Cupuaçu (CP)   |    Açai (AC)  |---       ')
+print(f'---|    P           {cp_p}
+      
+print(f'---|    M           {cp_m}                                 ')  
+
+print(f'---|    G           {cp_g}                                 ')  
+
+
